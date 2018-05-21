e1425d2027a615334608ce3c63cb63d9ed7442b8
Juanjo Ponz <juan.ponz@accessett.com>
2018-05-21 12:26:54 +0200

    Improve virtual machines up

--- virtual_machines_up.sh
+++ virtual_machines_up.sh
@@ -5,12 +5,42 @@
 #VIRTUAL_MACHINES_RELATIVE_HOMEPATH debe ser una carpeta dentro de $HOME donde se encuentran por carpetas distribuidas las maquinas virtuales, por ejemplo:
 # ./virtual_machines_up bla
 # levantara la máquina virtual dentro de $HOME/maquinasvirtuales/bla 
-if [ $# -ne 1 ]
+
+function usage {
+    echo "USAGE:"
+   echo "virtual_machines_up up NombreCarpetaQueContieneLaMaquina\n"
+   echo "Or"
+   echo "virtual_machine_up up NombrecarpetaQueContieneLaMaquina ssh => to open vagrant ssh connection"
+   echo "Or"
+   echo "virtual_machines_up list"
+}
+
+VIRTUAL_MACHINES_RELATIVE_HOMEPATH="maquinasvirtuales"
+
+if [ $# -gt 3 ]
 then
-   echo "Usage: virtual_machines_up NombreCarpetaQueContieneLaMaquina"
+   usage   
    exit
 fi
 
-VIRTUAL_MACHINES_RELATIVE_HOMEPATH="maquinasvirtuales"
-VIRTUAL_MACHINE=$1
-cd $HOME/$VIRTUAL_MACHINES_RELATIVE_HOMEPATH/$VIRTUAL_MACHINE && vagrant up
+case "$1" in
+    up)
+        VIRTUAL_MACHINE=$2
+        cd $HOME/$VIRTUAL_MACHINES_RELATIVE_HOMEPATH/$VIRTUAL_MACHINE && vagrant up
+        ;;
+    list)
+        ls -lha $HOME/$VIRTUAL_MACHINES_RELATIVE_HOMEPATH
+        ;;
+    *)
+        usage
+        exit
+esac
+
+case "$3" in
+    ssh)
+        vagrant ssh
+        ;;
+    *)
+        usage
+        exit
+esac
